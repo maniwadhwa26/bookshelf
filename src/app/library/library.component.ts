76f0700594d286a4f9782708520448a5771d8eac
Mani Wadhwa <maniwadhwa26@gmail.com>
2018-06-30 20:29:22 +0530

    Added Library Code

--- src/app/library/library.component.ts
+++ src/app/library/library.component.ts
@@ -1,16 +1,23 @@
-import {Component, OnInit} from "@angular/core";
-
+import {Component,OnInit} from "@angular/core";
+import {Book} from "../shared/book";
+import {Router, ActivatedRoute} from "@angular/router";
+import {GoogleBooksService} from "../shared/google-books.service";
+import { tap } from 'rxjs/operators';
+import { LibraryService } from "../shared/library.service";
 @Component({
   selector: 'app-library',
   templateUrl: './library.component.html',
   styleUrls: ['./library.component.css']
 })
-export class LibraryComponent implements OnInit {
+export class LibraryComponent implements OnInit{
 
-  constructor() {
+  private booklist:Book[];
+  constructor(private router: Router,private route: ActivatedRoute,private googlebookservice:GoogleBooksService,private libService:LibraryService) {
+   
   }
-
-  ngOnInit() {
+  ngOnInit(){
+    this.booklist = this.libService.books;
   }
+  
 
 }
