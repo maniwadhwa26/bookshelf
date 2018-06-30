b22ff42f2296670123f8081f5417debd4250d219
Mani Wadhwa <maniwadhwa26@gmail.com>
2018-06-30 18:19:08 +0530

    Added Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,12 @@
 import {Routes} from "@angular/router";
+import { SearchComponent } from "./search/search.component";
+import { LibraryComponent } from "./library/library.component";
+import { BookComponent } from "./book/book.component";
 
 export const routes: Routes = [
-  //TODO: Define the routes
+  
+    {path:'',redirectTo:'library',pathMatch:'full'},
+    {path:'search',component:SearchComponent},
+    {path:'library',component:LibraryComponent},
+    {path:'book/:bookId',component:BookComponent}
 ];
